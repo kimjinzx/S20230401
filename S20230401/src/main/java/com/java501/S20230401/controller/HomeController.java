57ce1edbc191705899c5db88f480fe10205dd381
MayB <MayB@DESKTOP-PSKJHVR>
2023-04-18 01:09:27 +0900

    Adding Multipart File data Upload

--- S20230401/src/main/java/com/java501/S20230401/controller/HomeController.java
+++ S20230401/src/main/java/com/java501/S20230401/controller/HomeController.java
@@ -8,11 +8,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.java501.S20230401.model.Article;
 import com.java501.S20230401.model.ArticleMember;
@@ -68,5 +73,19 @@ public class HomeController {
 	public String memberJoin(Model model) {
 		model.addAttribute("now", new Date());
 		return "joinForm";
+	}
+	
+	@PostMapping(value = "/joinProc")
+	public String memberJoinProcess(@RequestParam MultipartFile file, HttpServletRequest request) {
+		/*log.info("request={}", request);
+        log.info("itemName={}", itemName);
+        log.info("multipartFile={}", file);
+
+        if (!file.isEmpty()) {
+            String fullPath = fileDir + file.getOriginalFilename();
+            log.info("파일 저장 fullPath={}", fullPath);
+            file.transferTo(new File(fullPath));
+        }*/
+		return "redirect:/";
 	}
 }
