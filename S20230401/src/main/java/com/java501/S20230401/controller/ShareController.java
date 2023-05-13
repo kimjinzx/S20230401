36f415a6ea0397dd17357f478d36711a85e6f1b9
MayB <MayB@DESKTOP-PSKJHVR>
2023-05-13 11:33:01 +0900

    게시글 보기, 게시글 목록, 게시글 수정 변경... 컨트롤러 부분 일부 변경

--- S20230401/src/main/java/com/java501/S20230401/controller/ShareController.java
+++ S20230401/src/main/java/com/java501/S20230401/controller/ShareController.java
@@ -218,8 +218,16 @@ public class ShareController {
 		
 		// 글 정보 저장
 		Article detailArticle = articleService.detailShareArticle(article);
+		System.out.println(detailArticle);
 		// 지역 제한 조회
-		List<Region> regionList = regionService.dgRegionList();
+		//List<Region> regionList = regionService.dgRegionList();
+		// 이 아래는 꼭 들고 다녀야함!!!
+		Map<Region, List<Region>> regionHierachy = new HashMap<Region, List<Region>>();
+		List<Region> superRegions = regionService.getSuperRegions();
+		for (Region sups : superRegions) regionHierachy.put(sups, regionService.getChildRegions(sups.getReg_id()));
+		// 여기까지는 꼭 들고 다녀야함!!!
+		// 거래 참가자 목록 저장
+		List<Join> joinList = joinService.shareJoinList(detailArticle.getTrade().getTrd_id());
 		// 거래 상태
 		List<Comm> statusList = commService.commList(400);
 		// 카테고리 변경
@@ -228,8 +236,11 @@ public class ShareController {
 		model.addAttribute("categoryList", categoryList);
 		model.addAttribute("statusList", statusList);
 		model.addAttribute("article", detailArticle);
-		model.addAttribute("regionList", regionList);
+		//model.addAttribute("regionList", regionList);
+		model.addAttribute("superRegions", superRegions);
+		model.addAttribute("regions", regionHierachy);
 		model.addAttribute("category", category);
+		model.addAttribute("isAnyoneJoined", joinList == null || joinList.size() > 0);
 		
 		return "share/updateForm";
 	}
@@ -279,6 +290,10 @@ public class ShareController {
 		// trd_endDate 저장 (model 이슈 변수명 endDate로)
 		article.getTrade().setTrd_enddate(trd_endDate);
 		System.out.println(article.getArt_isnotice());
+		
+		// 거래 참가자 목록 저장
+		List<Join> joinList = joinService.shareJoinList(article.getTrade().getTrd_id());
+		if (joinList == null || joinList.size() > 0) article.setTrade(null);
 		
 		int result = tradeService.updateShare(article);
 		System.out.println("최종 결과 : "+result);
